58c2bad3786e56df43542af0fe57db6ed3fdae01
Mahyar_mac <Mahyar.rajabi94@gmail.com>
2025-01-11 21:03:08 -0500

    Remove unrelevent files

--- New Folder With Items/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use thermoML in a project::
-
-    import thermoml
--- New Folder With Items/src/thermoml/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for thermoML."""
-
-__author__ = """mahyar rajabi"""
-__email__ = 'mahyar.rajabi94@gmail.com'
-__version__ = '0.1.0'
